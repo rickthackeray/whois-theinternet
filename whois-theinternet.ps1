43182cd1bb3bc56ab3c2c0df5d8a6ed7e8068f54
rickthackeray <ricklikeswaffles@gmail.com>
2022-04-29 14:21:22 -0700

    Add files via upload

---
+++ whois-theinternet.ps1
@@ -0,0 +1,21 @@
+﻿$ip = "0.0.0.1"
+$results = @()
+while ($endip -ne "255.255.255.255") {
+    $result = Get-Whois $ip
+    Write-Output $result
+    $results += $result
+    $endip = $result[$result.Length-1].EndIP
+
+    # Manually count up IP
+    $octets = $endip.Split('.')
+    foreach ($i in 3..0) {
+        if ([int]$octets[$i] -lt 255) {
+            ([int]$octets[$i])++
+            break
+        }
+        else {
+            $octets[$i] = "0"
+        }
+    }
+    $ip = [String]::Join(".",$octets)
+}
